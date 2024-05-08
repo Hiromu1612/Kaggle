e70f2227936071a8365d8c9f9ecf1bbd27728a83
Hiromu1612 <150511546+Hiromu1612@users.noreply.github.com>
2024-05-08 23:42:43 +0900

    add

--- test2.py
+++ test2.py
@@ -39,20 +39,23 @@ print(model.score(X_test, y_test))
 
 
 #!提出 df_testのデータも同様に加工 
-X_test_submit=df_test[['Pclass','SibSp','Parch','Sex']]
-y_test_submit=df_test[['PassengerId']]
+X_test_submit=df_train[['Pclass','SibSp','Parch','Sex']]
+y_test_submit=df_train[['PassengerId']]
 
 X_test_submit=pd.get_dummies(X_test_submit, columns=["Pclass","Sex"])
 X_test_submit=X_test_submit.astype(np.int64)
 X_test_submit=pd.concat([X_test_submit, df_train["Fare"]], axis=1)
 X_test_submit["Fare"].fillna(df_test["Fare"].mean()) #Fareの欠損値を平均で埋める inplace=Trueで元のデータを更新
-print(X_test_submit.isnull().sum())
-print(X_test_submit.head())
 
+from sklearn.model_selection import train_test_split
+X_train, X_test, y_train, y_test=train_test_split(X_test_submit,y_test_submit, test_size=0.2, random_state=0)
+
+from sklearn.preprocessing import StandardScaler
 scaler=StandardScaler()
 scaler.fit(X_test_submit)
 X_test_submit=scaler.transform(X_test_submit)
 
-y_test_submit['Survived']=model.predict(X_test_submit) #submitにSurvived列を追加
-
-y_test_submit.to_csv("submit_2.csv", index=False) #index=Falseで行番号を出力しない
+from sklearn.neighbors import KNeighborsClassifier
+model=KNeighborsClassifier()
+model.fit(X_test_submit, y_test_submit)
+print(model.score(X_test_submit, y_test_submit))
